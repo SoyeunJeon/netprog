2c6b7aac4f15e976cc13ac8dcc24ef2c9bc2812f
s3622567 <soyeun.jeon42@gmail.com>
2020-05-17 10:45:47 +1000

    Add files via upload

---
+++ multi-netprog/Client.java
@@ -0,0 +1,92 @@
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class Client {
+	
+	private static final String HOST = "jupiter.csit.rmit.edu.au";
+	private static final int PORT = 61256;
+	
+	private DataInputStream dis;
+    private DataOutputStream dos;
+	
+	private BufferedReader brOut;
+	
+	public static void main(String[] args) {
+		new Client();
+	}
+	
+	public Client() {
+		
+		Socket client = null;
+				
+		try {
+			while(true) {
+				client = new Socket(HOST, PORT);
+				
+				// init input and output streams and reader to get user input
+				dis = new DataInputStream(client.getInputStream());
+				dos = new DataOutputStream(client.getOutputStream());
+				
+				brOut = new BufferedReader(new InputStreamReader(System.in));
+			
+				// get msg from server that will register player with their first name
+				System.out.println("<<<<<-- Welcome to Guessing Game -->>>>>");
+				System.out.println("Game will start soon. Please wait....");
+				System.out.println(dis.readUTF());
+				dos.writeUTF(brOut.readLine());
+				
+				// play guessing game, getting msg from server and send the response back 
+				System.out.println("<- Game Started ->");
+				String msg = "";
+				while (true) {
+					msg = dis.readUTF();
+					System.out.println(msg);
+					
+					Timer timer = new Timer();
+					timer.scheduleAtFixedRate(new Reminder(), 5000, 5000);
+					
+					if (msg.contains("Correct!") || msg.contains("failed")) { // game finish after 4 attempts or success
+						break;
+					}
+					dos.writeUTF(brOut.readLine());
+					timer.cancel();
+				}
+				
+				// ask user to quit game or play new game
+				System.out.println(dis.readUTF());
+				msg = brOut.readLine();
+				dos.writeUTF(msg);
+				if (msg.equals("q")) {
+					System.exit(0);
+				} else if (msg.equals("p")) {
+					
+				}
+			}
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+	          if(client != null) client.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	// reminder class to send notification to client to response
+	class Reminder extends TimerTask {
+	    public void run() {
+	       System.out.println("Please enter your response."); 
+	    }
+	}
+}
---
+++ multi-netprog/ClientHandler.java
@@ -0,0 +1,134 @@
+import java.io.*;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.Socket;
+import java.net.SocketException;
+import java.util.Random;
+
+public class ClientHandler extends Thread {
+
+    private Socket connection;
+	private DataInputStream dis;
+    private DataOutputStream dos;
+    
+    public static int MAX_GUESS = 4;
+    private int guess_counter = 0;
+    private int answer;
+    private String playerN;
+    
+
+    public ClientHandler(Socket connection, int answer)
+    {
+        this.connection = connection;
+        this.answer = answer;
+    }
+
+    @Override
+    public void run() {
+    	
+        try {
+        	// init input and output stream
+            dis = new DataInputStream(connection.getInputStream()); 
+            dos = new DataOutputStream(connection.getOutputStream());
+            
+            // register player by their name
+            dos.writeUTF("Player name? = ");
+            playerN = dis.readUTF();
+            System.out.println(playerN + " started guessing game.");
+            Server.writeLog("SERVER>> " + playerN + " registered.");
+            
+			System.out.println("Generated random number is : "+answer);
+			
+			gameBody();
+			
+			// when the game finishes, add the player in the ranking list
+			Server.addRank(playerN);
+			
+			// ask client to play new game or quit the game
+			dos.writeUTF("Do you want to play again? Enter 'p' to play again or 'q' to quit: ");
+			String resp = dis.readUTF();
+			if (resp.equals("p")) {
+				System.out.println("play again");
+				Server.joinQueue(connection);
+			} else if (resp.equals("q")) {
+				System.out.println("quit");
+			}
+			
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                dis.close();
+                dos.close();
+                connection.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        
+        
+    }
+    
+    public void gameBody() {
+    	try {
+    		
+			dos.writeUTF("Guess the number between 0 and 12: ");
+			Server.writeLog("SERVER>> Guess the number:");
+			
+			// ask clients to guess the number, max guess is 4 and it will ends after 4 attempts if each answer is wrong
+			while(guess_counter < MAX_GUESS) {
+				guess_counter++;
+				
+				int guessed = validate(dis.readUTF());
+				
+				if (guessed == -1) { // invalid input
+					dos.writeUTF("Invalid input. Please enter your guess again: ");
+					Server.writeLog("SERVER>> Invalid input. Please enter your guess again:");
+					guess_counter--;
+					continue;
+				}
+				
+				System.out.println("[" + playerN + "'s attempt" + guess_counter + "] " + "Player guessed number = " + guessed);
+				Server.writeLog("[" + playerN + "'s attempt" + guess_counter + "] " + "Player guessed number = " + guessed);
+				
+				if (guessed == answer) {
+					dos.writeUTF("Correct!");
+					Server.writeLog("SERVER>> Correct!");
+					
+					System.out.println(playerN + " finished game.");
+					Server.writeLog("SERVER>> " + playerN + " finished game. " + Server.timeLog());
+					break;
+				} else if (guessed > answer && guess_counter != 4){ // client response is bigger than the answer
+					dos.writeUTF("Answer is smaller. Guess attempt " + guess_counter + ": ");
+					Server.writeLog("SERVER>> Answer is smaller. Guess attempt " + guess_counter + ": ");
+				} else if (guessed < answer && guess_counter != 4) { // client response is smaller than the answer
+					dos.writeUTF("Answer is bigger. Guess attempt " + guess_counter + ": ");
+					Server.writeLog("SERVER>> Answer is bigger. Guess attempt " + guess_counter + ": ");
+				} else if (guess_counter == 4) {
+					dos.writeUTF("No more guess counter left. Guess failed.");
+					Server.writeLog("SERVER>> No more guess counter left. Guess failed.");
+					
+					System.out.println(playerN + " finished game.");
+					Server.writeLog("SERVER>> " + playerN + " finished game. " + Server.timeLog());
+					break;
+				}
+			}
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+    }
+    
+    // validate the input to make sure its only in number between 0 to 12
+	public int validate(String input) {
+		String regex = "[0-9]+";
+		if (input.matches(regex)) {
+			int temp = Integer.parseInt(input);
+			if (temp <= 12 && temp >= 0) {
+				return temp;
+			}
+		} 
+		return -1;
+	}
+	
+}
